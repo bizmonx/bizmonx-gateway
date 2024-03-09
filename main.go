5d5bf44aa00e664f877c120dee14df391428d241
jeroenflvr <jeroen@flexworks.eu>
2024-03-09 07:35:40 +0100

    feat: send on data channel

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
+	"time"
 
 	x "github.com/bizmonx/bizmonx-gateway/xymon"
 )
@@ -16,10 +20,21 @@ var h *x.XymonHost
 
 func main() {
 	host := os.Getenv("XYMON_HOST")
+	if host == "" {
+		log.Println("XYMON_HOST not set, using localhost")
+		host = "localhost"
+	}
+
 	port := os.Getenv("XYMON_PORT")
-	if host == "" || port == "" {
-		log.Fatal("XYMON_HOST and XYMON_PORT must be set")
-		os.Exit(1)
+	if port == "" {
+		log.Println("XYMON_PORT not set, using 1984")
+		port = "1984"
+	}
+
+	serverPort := os.Getenv("XYMON_GATEWAY_SERVER_PORT")
+	if serverPort == "" {
+		log.Println("XYMON_GATEWAY_SERVER_PORT not set, using 1976")
+		serverPort = "1976"
 	}
 
 	p, err := strconv.Atoi(port)
@@ -29,14 +44,35 @@ func main() {
 
 	h = &x.XymonHost{Host: host, Port: p}
 
-	http.HandleFunc("/", handler)
+	//http.HandleFunc("/", handler)
+	http.HandleFunc("/status", statusHandler)
+	http.HandleFunc("/data", dataHandler)
 
-	log.Println("Listening on :1976...")
-	log.Fatal(http.ListenAndServe(":1976", nil))
+	log.Println("Listening on :" + serverPort + "...")
+	server := &http.Server{Addr: ":" + serverPort, Handler: nil}
+
+	//log.Fatal(http.ListenAndServe(":1976", nil))
+
+	// Set up channel to receive OS signals
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until a signal is received
+	<-stopChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	server.Shutdown(ctx)
+
+	fmt.Println("Shutting down gracefully...")
 
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func statusHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
 		return
@@ -50,7 +86,33 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	var msg x.XymonMessage
+	var msg x.StatusMessage
+	err = json.Unmarshal(body, &msg)
+	if err != nil {
+		http.Error(w, "Error parsing request body", http.StatusInternalServerError)
+		return
+	}
+	msg.Send(h)
+
+	// Now forward this message to the Xymon server
+
+	fmt.Fprintln(w, "Message forwarded successfully to Xymon")
+}
+
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	// Read the body of the POST request
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	var msg x.DataMessage
 	err = json.Unmarshal(body, &msg)
 	if err != nil {
 		http.Error(w, "Error parsing request body", http.StatusInternalServerError)
--- xymon/xymon.go
+++ xymon/xymon.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-type XymonMessage struct {
+type StatusMessage struct {
 	Message  string `json:"message"`
 	Color    string `json:"color"`
 	Test     string `json:"test"`
@@ -14,34 +14,61 @@ type XymonMessage struct {
 	Lifetime string `json:"lifetime"`
 }
 
+type DataMessage struct {
+	Message  string `json:"message"`
+	Host     string `json:"host"`
+	DataName string `json:"data_name"`
+}
+
 type XymonHost struct {
 	Host string `json:"host"`
 	Port int    `json:"port"`
 }
 
-func (x *XymonMessage) Stringify() string {
+func (x *StatusMessage) Stringify() string {
 	lifetime := map[bool]string{true: "+" + x.Lifetime, false: ""}[x.Lifetime != ""]
 	m := fmt.Sprintf("status%s %s.%s %s %s ", lifetime, x.Host, x.Test, x.Color, x.Message)
 	fmt.Print(m)
 	return m
 }
 
-func (x *XymonMessage) MarshalJSON() ([]byte, error) {
+func (x *DataMessage) Stringify() string {
+	m := fmt.Sprintf("data %s.%s\n%s ", x.Host, x.DataName, x.Message)
+	fmt.Print(m)
+	return m
+}
+
+func (x *DataMessage) Send(host *XymonHost) error {
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host.Host, host.Port))
+	if err != nil {
+		fmt.Printf("Error connecting to %s:%d: %s\n", host.Host, host.Port, err.Error())
+		return err
+	}
+	defer conn.Close()
+
+	// Send the message
+	_, err = conn.Write([]byte(x.Stringify()))
+	if err != nil {
+		fmt.Println("Error sending message:", err.Error())
+		return err
+	}
+
+	fmt.Println("Message successfully sent to Xymon.")
+	return nil
+}
+
+func (x *StatusMessage) MarshalJSON() ([]byte, error) {
 	return []byte(`"` + x.Message + `"`), nil
 }
 
-func (x *XymonMessage) Send(h *XymonHost) error {
+func (x *StatusMessage) Send(h *XymonHost) error {
 	conn, err := net.Dial("tcp", "localhost:1984")
 	if err != nil {
-		// Handle error
 		fmt.Println("Error connecting:", err.Error())
 		os.Exit(1)
 	}
 	// Close the connection when the function exits
 	defer conn.Close()
-
-	// Message to send
-	///message := "status newhost.go green message from go client\n"
 
 	// Send the message
 	_, err = conn.Write([]byte(x.Stringify()))
@@ -51,6 +78,6 @@ func (x *XymonMessage) Send(h *XymonHost) error {
 		return err
 	}
 
-	fmt.Println("Message sent successfully")
+	fmt.Println("Message successfully sent to Xymon.")
 	return nil
 }
